a788630abade44ea32ddb00c0ab89a7ca0474367
lsh58 <60163978+lsh58@users.noreply.github.com>
2020-05-05 11:41:46 +0900

    ,

--- js/script.js
+++ js/script.js
@@ -1,34 +1,29 @@
 
 $(function () {
+
     $('header ul').mouseenter(navopen);
     $('header ul').mouseleave(navclose);
-
+    
     function navopen(){
         $('header ul li').css({
-            borderLeft: "1px solid #333",
             paddingBottom: "20px"
         });
         $('header ul li nav').css({
             display:"flex"
         });
-        $('header ul li nav').animate({
+        $('header ul li nav').css({
             height:"100px"
-        },500);
+        });
     }
     function navclose(){
         $('header ul li').css({
-            borderLeft: "none",
             paddingBottom: "0"
         });
-        $('header ul li nav').animate({
+        $('header ul li nav').css({
             height:"0"
-        },500);
-        setTimeout(function(){
-            $('header ul li nav').css({
-                display:"none"
-            });
-        },400);
+        });
+        $('header ul li nav').css({
+            display:"none"
+        });
     }
-
-    
 });
