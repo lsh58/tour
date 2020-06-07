416d03c5844dd2e6d05ebcfe85990ded9359b25f
lsh58 <lsh58@gmail.com>
2020-06-08 01:29:18 +0900

    .

--- js/script.js
+++ js/script.js
@@ -10,5 +10,15 @@ $(function () {
         $('.popMenu').css({
             top: '-100vh'
         });
-    });
+    }); //버거메뉴 열고닫기
+
+    var ulSize = parseInt($('.footer__dropBox ul').css('height'));
+    $('.footer__dropBox ul').css({
+        top:ulSize*-1
+    });//ul 높이값 주기
+    $('.footer__dropBox ul').fadeOut();
+    $('.footer__dropBox__button').click(function(){
+        $('.footer__dropBox ul').slideDown();
+    });//애니메이션 다시 생각해볼것
+
 });
