18a29114db2651526dfe81dbae6a40fde5fdda00
lsh58 <lsh58@gmail.com>
2020-06-11 00:47:52 +0900

    .

--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
 
 $(function () {
 
-    $('.header__burger').click(function () {
+    burger(); //버거메뉴 열고닫기
+    dropBox(); //드롭박스 열고닫기
+
+
+});
+
+function burger(){ //버거메뉴 열고닫기
+    var bln = true;
+    $('.burger').click(function () {
+        if(bln==true){
+        bln=false;
         $('.popMenu').css({
             top: 0
         });
-    });
-    $('.popMenu__burger').click(function () {
+        $('.burger span').eq(0).css({
+            transform: "rotate(45deg) translateY(10px)"
+        })
+        $('.burger span').eq(1).css({
+            opacity: 0
+        })
+        $('.burger span').eq(2).css({
+            transform: "rotate(-45deg) translateY(-10px)"
+        })
+    }
+    else{
+        bln=true;
         $('.popMenu').css({
             top: '-100vh'
         });
-    }); //버거메뉴 열고닫기
+        $('.burger span').eq(0).css({
+            transform: "rotate(0) translateY(0)"
+        })
+        $('.burger span').eq(1).css({
+            opacity: 1
+        })
+        $('.burger span').eq(2).css({
+            transform: "rotate(0) translateY(0)"
+        })
+    }
+    });
+}
 
-    var ulSize = parseInt($('.footer__dropBox ul').css('height'));
-    $('.footer__dropBox ul').css({
-        top:ulSize*-1
-    });//ul 높이값 주기
-    $('.footer__dropBox ul').fadeOut();
+function dropBox(){ //드롭박스 열고닫기
+    var bln = true;
+    var dropBox = $('.footer__dropBox ul li');
     $('.footer__dropBox__button').click(function(){
-        $('.footer__dropBox ul').slideDown();
-    });//애니메이션 다시 생각해볼것
-
-});
+        if(bln == true){
+            bln=false;
+            for(var i=0; i<dropBox.length; i++){
+            $('.footer__dropBox ul li').eq(i).css({
+                top:-40*(dropBox.length-i)+"px",
+                });
+            }
+        }
+        else{
+            bln=true;
+            for(var i=0; i<dropBox.length; i++){
+            $('.footer__dropBox ul li').eq(i).css({
+                top:0,
+            });
+            }
+        }
+    })
+}
