6368d2faa7c9c618f17cf3bc09f4e2f194652ba3
lsh58 <lsh58@gmail.com>
2020-06-11 22:44:42 +0900

    .

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ $(function () {
 
     burger(); //버거메뉴 열고닫기
     dropBox(); //드롭박스 열고닫기
-
-
+    header(); //헤더 열고닫기
+    rightBox();//배너슬라이드
+    $('.main__board__category button').click(function(){
+        $('.main__board__category button').removeClass('selected');
+        $(this).addClass('selected');
+    })
 });
 
 function burger(){ //버거메뉴 열고닫기
@@ -64,4 +68,80 @@ function dropBox(){ //드롭박스 열고닫기
             }
         }
     })
+}
+
+function header(){//헤더 열고닫기
+    $('.header ul').mouseenter(function(){
+        $('.header ul li nav').css({
+        visibility : "visible"
+        })
+    });
+    $('.header ul').mouseover(function(){
+        $('.header ul li nav').css({
+        visibility : "visible"
+        });
+    });
+    $('.header ul').mouseout(function(){
+        $('.header ul li nav').css({
+        visibility : "hidden"
+        });
+    });
+}
+
+function rightBox(){//배너슬라이드
+    var slideAni;
+    var bln=true;
+    slide();
+    $('.main__rightBox__banner button').eq(0).click(function(){ //왼쪽버튼
+        $('.main__rightBox__banner ul').prepend($('.main__rightBox__banner ul li').eq(3));
+        $('.main__rightBox__banner ul').css({
+            left:'-100%'
+        });
+        $('.main__rightBox__banner ul').animate({
+            left:'0'
+        });
+    });
+    $('.main__rightBox__banner button').eq(1).click(function(){ //가운데버튼 (일시정지/플레이)
+        if(bln==true){
+            bln=false;
+            clearInterval(slideAni);
+            $('.rightBox__stop').css({
+                display:"none"
+            });
+            $('.rightBox__play').css({
+                display:"block"
+            });
+        }else{
+            bln=true;
+            slide();
+            $('.rightBox__stop').css({
+                display:"block"
+            });
+            $('.rightBox__play').css({
+                display:"none"
+            });
+        }
+    });
+    $('.main__rightBox__banner button').eq(2).click(function(){ //오른쪽버튼
+        $('.main__rightBox__banner ul').animate({
+            left:'-100%'
+        }, function () {
+            $('.main__rightBox__banner ul').append($('.main__rightBox__banner ul li').eq(0));
+            $('.main__rightBox__banner ul').css({
+                left:0
+            });
+        });
+    });
+    function slide(){
+    slideAni = setInterval(function(){
+            $('.main__rightBox__banner ul').animate({
+                left:'-100%'
+            }, function () {
+                $('.main__rightBox__banner ul').append($('.main__rightBox__banner ul li').eq(0));
+                $('.main__rightBox__banner ul').css({
+                    left:0
+                });
+            });
+        },2000);
+    }
 }
